dc58f60b36f62307ac7015975c022c7adcb2fd0a
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-04-12 10:27:22 +0200

    try different dir

--- src/routes/test.json.js
+++ src/routes/test.json.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 export function get() {
 	const paths = [];
 
-	fs.readdir('./', (err, files) => {
+	fs.readdir('../../documentation/docs', (err, files) => {
 		files.forEach((file) => {
 			console.log(file);
 			paths.push(file);
