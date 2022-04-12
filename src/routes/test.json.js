6d5e24d9f8392c5e42ae9dee733598f137fab365
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-04-12 10:42:01 +0200

    b

--- src/routes/test.json.js
+++ src/routes/test.json.js
@@ -3,13 +3,25 @@ import fs from 'fs';
 export function get() {
 	const files = [];
 
+	console.log('../docs');
 	for (const file of fs.readdirSync(`../documentation`)) {
-		files.push(file);
+		console.log(file);
+	}
+
+	console.log('./');
+	for (const file of fs.readdirSync(`./`)) {
+		console.log(file);
+	}
+
+	console.log('../');
+	for (const file of fs.readdirSync(`../`)) {
+		console.log(file);
 	}
 
 	return {
 		body: {
-			files
+			files,
+			test: 'lorem ipsum'
 		}
 	};
 }
