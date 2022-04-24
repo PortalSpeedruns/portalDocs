c555ed5b313bc1ba40463bd132ae2021502a96ed
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-04-24 23:01:36 +0200

    feat: add 404 page for gh pages

---
+++ src/routes/404.svelte
@@ -0,0 +1,8 @@
+<script>
+	import ErrorPage from '$lib/errorPage.svelte';
+</script>
+
+<ErrorPage status={404} />
+
+<style lang="scss">
+</style>
--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -10,48 +10,13 @@
 </script>
 
 <script>
+	import ErrorPage from '$lib/errorPage.svelte';
+
 	export let error;
 	export let status;
 </script>
 
-<main>
-	<div>
-		{#if status === 404}
-			<h1>4 <span>0</span> 4</h1>
-			<p>You're not meant to be here 😳😂</p>
-		{:else}
-			<h1>{status}</h1>
-			<p>{error.message}</p>
-		{/if}
-	</div>
-</main>
+<ErrorPage {status} {error} />
 
 <style lang="scss">
-	main {
-		height: 100vh;
-		display: flex;
-		align-items: center;
-
-		padding-top: var(--top-offset);
-		padding-bottom: var(--top-offset);
-
-		background-image: url('/images/404background.png');
-		background-size: cover;
-	}
-
-	div {
-		max-width: var(--linemax);
-		width: var(--linemax);
-		margin-left: auto;
-		margin-right: auto;
-		color: white;
-	}
-
-	h1 {
-		font-size: 5.5rem;
-	}
-
-	p {
-		font-size: 3.5rem;
-	}
 </style>
