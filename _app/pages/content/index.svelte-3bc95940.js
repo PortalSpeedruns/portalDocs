bf546b483e465ed48fc8b15aba518442de873f8d
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-05-01 17:57:52 +0000

    Deploying to gh-pages from @ PortalSpeedruns/portalDocs@d654c1f1991906b0cdd976d5a6364d21c948e491 🚀

---
+++ _app/pages/content/index.svelte-3bc95940.js
@@ -0,0 +1,1 @@
+import{S as e,i as n,s}from"../../chunks/index-a86ccbc9.js";const a=!0;function u(){return{status:307,redirect:"/content.json"}}class l extends e{constructor(t){super(),n(this,t,null,null,s,{})}}export{l as default,u as load,a as prerender};
