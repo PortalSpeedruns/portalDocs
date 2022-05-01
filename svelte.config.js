13c75d00f56d38baec2005fb43cf7c7de29f42fa
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-05-01 19:55:45 +0200

    feat: setup github pages workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from 'svelte-preprocess';
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
